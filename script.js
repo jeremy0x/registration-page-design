54490a782d5bb0c03965c319eacbb5771ce03e75
Jeremiah Aworetan <aworetanjeremiah@gmail.com>
2022-06-20 19:46:07 +0100

    no message

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-const input = document.getElementById('input');
-const form = document.getElementById('form');
+// const input = document.getElementById('input');
+// const form = document.getElementById('form');
 
-form.addEventListener('submit', (e) => {
-  if (input.value === '' || input.value == null) {
-    e.preventDefault()
-    document.getElementById('error').style.visibility = 'visible'
-  }
-})
+// form.addEventListener('submit', (e) => {
+//   if (input.value === '' || input.value == null) {
+//     e.preventDefault()
+//     document.getElementById('error').style.visibility = 'visible'
+//   }
+// })
